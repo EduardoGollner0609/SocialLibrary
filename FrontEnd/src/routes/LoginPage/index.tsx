09ae9d3ea200184a4235ad04f503453ac4b0f90b
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-22 02:16:12 -0300

    Adicionando estilos ao LoginPage.

--- FrontEnd/src/routes/LoginPage/index.tsx
+++ FrontEnd/src/routes/LoginPage/index.tsx
@@ -1,14 +1,15 @@
+import "./styles.css";
 import imgAnimatedHomePage from "../../assets/AnimatedHomePage.svg";
 import CardLogin from "../../components/CardLogin";
 
 export default function LoginPage() {
   return (
-    <section>
-      <div>
+    <section id="section-login-page">
+      <div id="login-page-left">
         <img src={imgAnimatedHomePage} alt="" />
       </div>
-      <div>
-      <CardLogin />
+      <div id="login-page-right">
+        <CardLogin />
       </div>
     </section>
   );
