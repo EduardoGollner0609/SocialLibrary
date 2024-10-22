5b37bff9b62f80110c11005ff2dfe9334fffb5b7
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-22 00:39:42 -0300

    Criando HTML do Header.

--- FrontEnd/src/components/Header/index.tsx
+++ FrontEnd/src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 export default function Header() {
-    return (
-   <header>
-    <nav>
-        <h1>SocialLibrary</h1>
-    </nav>
-   </header>
-    );
+  return (
+    <header>
+      <nav>
+        <h1>Social Library</h1>
+        <ul>
+          <li>
+            <a href="">Entrar</a>
+          </li>
+          <li>
+            <a href="">Mais Detalhes</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
