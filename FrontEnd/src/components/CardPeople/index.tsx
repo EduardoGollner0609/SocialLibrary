1ce8c3833f3aed6356ffa9b198493c334a67070b
EduGollner0609 <dudugollner@gmail.com>
2024-10-25 00:28:00 -0300

    Criando função para chamar o card.

--- FrontEnd/src/components/CardPeople/index.tsx
+++ FrontEnd/src/components/CardPeople/index.tsx
@@ -2,10 +2,18 @@ import "./styles.css";
 import imgPessoa1 from "../../assets/foto-gc.jpg";
 
 export default function CardPeople() {
+  
+  function cardPeopleDetailShow() {
+    const cardPeopleDetails = document.querySelector(".card-people-details");
+    if (cardPeopleDetails != null) {
+      cardPeopleDetails.classList.add("card-active");
+    }
+  }
+
   return (
-      <div className="card-people">
-        <img src={imgPessoa1} alt="" />
-        <p>GC BarberShop</p>
-      </div>
+    <div className="card-people" onClick={cardPeopleDetailShow}>
+      <img src={imgPessoa1} alt="" />
+      <p>GC BarberShop</p>
+    </div>
   );
 }
--- FrontEnd/src/routes/SocialSearch/index.tsx
+++ FrontEnd/src/routes/SocialSearch/index.tsx
@@ -4,6 +4,7 @@ import SearchBar from "../../components/SearchBar";
 import "./styles.css";
 
 export default function SocialHome() {
+
   return (
     <main>
       <section id="section-social-search">
