530f087a58f5c5d19755f5f42317a46f979c98b2
DuduDeploys <dudugollner6897@gmail.com>
2024-10-24 23:41:25 -0300

    Adicionando HTML.

--- FrontEnd/src/components/CommunityDetails/index.tsx
+++ FrontEnd/src/components/CommunityDetails/index.tsx
@@ -1,9 +1,44 @@
 import "./styles.css";
+import imgCommunity from "../../assets/foto-gc.jpg";
 
 export default function PeopleDetails() {
   return (
     <div className="card-community-details">
-   
+      <h2>GC BarberShop</h2>
+      <div className="card-community-details-top">
+        <div className="community-credentials">
+          <div className="credentials-item">
+            <h3>Nome:</h3>
+            <p>Lindões</p>
+          </div>
+          <div className="credentials-item">
+            <h3>Quantidade de membros: </h3>
+            <p>02/50</p>
+          </div>
+          <div className="credentials-item">
+            <h3>Data de criação: </h3>
+            <p>07/02/2024</p>
+          </div>
+        </div>
+
+        <img src={imgCommunity} alt="" />
+      </div>
+
+      <div className="card-community-details-bottom">
+        <div className="community-description">
+          <h3>Sobre: </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur, deserunt vel ullam quisquam unde excepturi laudantium,
+            similique totam odit reiciendis inventore? Recusandae praesentium
+            magnam iusto dolorum, adipisci cum autem necessitatibus.
+          </p>
+        </div>
+
+        <div className="card-people-details-btns">
+          <button>Entrar</button>
+        </div>
+      </div>
     </div>
   );
 }
