797e3569711e754297245b2f0617c577a10fe506
EduGollner0609 <dudugollner@gmail.com>
2024-10-25 03:18:01 -0300

    Adicionando estilo para box-title como global.

--- FrontEnd/src/routes/ReadingCorner/index.tsx
+++ FrontEnd/src/routes/ReadingCorner/index.tsx
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export default function ReadingCorner() {
-  return <h1>Leitura</h1>;
+  return (
+    <main>
+      <section id="section-reading-corner">
+        <div className="box-title">
+          <h1>Cantinho da leitura</h1>
+        </div>
+      </section>
+    </main>
+  );
 }
