26e9f9a0e580f93e621a75a1676768bfd26e4e83
EduGollner0609 <dudugollner@gmail.com>
2024-10-25 01:47:45 -0300

    Alinhando estilos que são semelhantes.

--- FrontEnd/src/components/CardCommunity/index.tsx
+++ FrontEnd/src/components/CardCommunity/index.tsx
@@ -1,19 +1,20 @@
 import "./styles.css";
-import imgCommunity from '../../assets/foto-gc.jpg';
+import imgCommunity from "../../assets/foto-gc.jpg";
 
 export default function CardCommunity() {
-
   function cardCommunityDetailsShow() {
-    const cardCommunityDetails = document.querySelector(".card-community-details");
+    const cardCommunityDetails = document.querySelector(
+      ".card-community-details"
+    );
     if (cardCommunityDetails != null) {
       cardCommunityDetails.classList.add("card-community-active");
     }
   }
   return (
-  <div className="card-community" onClick={cardCommunityDetailsShow}>
-<img src={imgCommunity} alt="" />
-<p>Nome da comunidade</p>
-<p>0/50</p>
-  </div>
+    <div className="card-community" onClick={cardCommunityDetailsShow}>
+      <img src={imgCommunity} alt="" />
+      <p>Nome da comunidade</p>
+      <p>0/50</p>
+    </div>
   );
 }
