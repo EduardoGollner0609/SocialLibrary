fb2d44f2ffd414466bbfcce260e8045df30871bd
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 05:57:35 -0300

    Criando projeto Spring para backend.

---
+++ BackEnd/src/main/java/com/eduardo/sociallibrary/SociallibraryApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.sociallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociallibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociallibraryApplication.class, args);
+	}
+
+}
